c12fc33a80c1a246141a1715687134fabb0041ab
local-eric-zhou <mengyilingjian@outlook.com>
2019-02-25 22:39:49 +0800

    新建表单创建

--- resources/views/projects/_createModal.blade.php
+++ resources/views/projects/_createModal.blade.php
@@ -7,19 +7,33 @@
         <div class="modal fade" id="createProjectModal" tabindex="-1" role="dialog" aria-labelledby="createProjectModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="createProjectModalLabel">Modal title</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    ...
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
-                </div>
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="createProjectModalLabel">新建项目</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    {!! Form::open(['url'=>'/projects']) !!}
+                    <div class="modal-body">
+
+                        <div class="form-group">
+                            {!! Form::label('name', '项目名称') !!}
+
+                            {!! Form::text('name', '', ['class'=>'form-control']) !!}
+
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('thumbnail', '项目缩略图') !!}
+
+
+                            {!! Form::file('thumbnail', ['class'=>'form-control-file']) !!}
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        {!! Form::submit('新建表单', ['class'=>'btn btn-primary']) !!}
+                    </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
