796e448ff7de91d983521820fe86a0ab45bdc698
local-eric-zhou <mengyilingjian@outlook.com>
2019-02-25 21:37:53 +0800

    npm run dev 重新编译bootstrap前端资源

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -25,6 +25,14 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
+try{
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+    require('bootstrap');
+} catch (e){
+
+}
+
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
